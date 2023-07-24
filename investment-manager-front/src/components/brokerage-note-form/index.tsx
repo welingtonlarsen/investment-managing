69cae6dc0f4478585267311a41ae20be9131ca49
Welington Larsen <welington.larsen@sanar.com>
2023-07-24 19:29:25 -0300

    feat: implement orders usecase

--- investment-manager-front/src/components/brokerage-note-form/index.tsx
+++ investment-manager-front/src/components/brokerage-note-form/index.tsx
@@ -1,8 +1,9 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
-import { Button, Typography } from '@mui/material';
+import { Button, Grid, IconButton, Typography } from '@mui/material';
 import { useFieldArray, useForm } from 'react-hook-form';
+import AddCircleIcon from '@mui/icons-material/AddCircle';
 
 
 type FormValues = {
@@ -14,11 +15,22 @@ type FormValues = {
         buyOrSell: string,
         marketType: string,
         title: string,
-        quantity: number,
-        price: number,
-        total: number,
+        quantity: number | undefined,
+        price: number | undefined,
+        total: number | undefined,
         debitOrCredit: string
     }[]
+}
+
+const defaultOrder = {
+    market: '',
+    buyOrSell: '',
+    marketType: '',
+    title: '',
+    quantity: undefined,
+    price: undefined,
+    total: undefined,
+    debitOrCredit: ''
 }
 
 const BrokerageNoteForm = () => {
@@ -29,16 +41,7 @@ const BrokerageNoteForm = () => {
             date: '',
             client: '',
             orders: [
-                {
-                    market: '',
-                    buyOrSell: '',
-                    marketType: '',
-                    title: '',
-                    quantity: undefined,
-                    price: undefined,
-                    total: undefined,
-                    debitOrCredit: ''
-                }
+                defaultOrder
             ]
         }
     })
@@ -54,43 +57,74 @@ const BrokerageNoteForm = () => {
         console.log(data);
     }
 
+    const onIncrementOrder = () => {
+        append(defaultOrder);
+    }
+
     const renderGeneralInformation = () => {
         return (
-            <Box sx={{maxWidth: '700px'}}>
+            <Box>
                 <Typography sx={{mb: 2}} variant="h6" gutterBottom>
                     Informações gerais
                 </Typography>
-                <Box sx={{display: 'flex', justifyContent: 'space-between', flexDirection: {xs: 'column', sm: 'row'}}}>
-                    <TextField {...register('number')} InputLabelProps={{shrink: true}} sx={{mb: 2}} id="outlined-basic" label="Número" variant="outlined" />
-                    <TextField {...register('date')} InputLabelProps={{shrink: true}} sx={{mb: 2}} id="outlined-basic" label="Data" variant="outlined" />
-                    <TextField {...register('client')} InputLabelProps={{shrink: true}} sx={{mb: 2}} id="outlined-basic" label="Cliente" variant="outlined" />
-                </Box>
+                <Grid container spacing={2}>
+                    <Grid item xs={12} sm={6} lg={2}>
+                        <TextField sx={{display: 'flex'}} {...register('number')} InputLabelProps={{shrink: true}} id="outlined-basic" label="Número" variant="outlined" />
+                    </Grid>
+                    <Grid item xs={12} sm={6} lg={2}>
+                        <TextField sx={{display: 'flex'}} {...register('date')} InputLabelProps={{shrink: true}} id="outlined-basic" label="Data" variant="outlined" />
+                    </Grid>
+                    <Grid item xs={12} sm={6} lg={2}>
+                        <TextField sx={{display: 'flex'}} {...register('client')} InputLabelProps={{shrink: true}} id="outlined-basic" label="Cliente" variant="outlined" />
+                    </Grid>
+                </Grid>
             </Box>
         )
     }
 
     const renderOrders = () => {
         return (
-            <Box>
+            <Box sx={{display: 'flex', flexDirection: 'column'}}>
                 <Typography sx={{my: 2}} variant="h6" gutterBottom>
                     Ordens
                 </Typography>
                 {
-                    fields.map((field, index) => {
+                    fields.map((_, index) => {
                             return (
-                                <Box key={index} sx={{display: 'flex', justifyContent: 'space-between'}}>
-                                    <TextField inputProps={{...register(`orders.${index}.market`)}} label="Mercado" variant="outlined" InputLabelProps={{shrink: true}} />
-                                    <TextField inputProps={{...register(`orders.${index}.buyOrSell`)}} label="C/V" variant="outlined" sx={{maxWidth: 100}} InputLabelProps={{shrink: true}} />
-                                    <TextField inputProps={{...register(`orders.${index}.marketType`)}} label="Tipo mercado" variant="outlined" InputLabelProps={{shrink: true}} />
-                                    <TextField inputProps={{...register(`orders.${index}.title`)}} label="Título" variant="outlined" InputLabelProps={{shrink: true}} />
-                                    <TextField inputProps={{...register(`orders.${index}.quantity`)}} label="Quantidade" variant="outlined"  sx={{maxWidth: 100}} InputLabelProps={{shrink: true}} />
-                                    <TextField inputProps={{...register(`orders.${index}.price`)}} label="Preço" variant="outlined"  sx={{maxWidth: 150}} InputLabelProps={{shrink: true}} />
-                                    <TextField inputProps={{...register(`orders.${index}.total`)}} label="Total" variant="outlined"  sx={{maxWidth: 150}} InputLabelProps={{shrink: true}} />
-                                    <TextField inputProps={{...register(`orders.${index}.debitOrCredit`)}} label="D/C" variant="outlined"  sx={{maxWidth: 100}} InputLabelProps={{shrink: true}} />
-                                </Box>
+                                    <Grid key={index} sx={{mb: 4}} container spacing={2} >
+                                        <Grid item xs={12} sm={6} lg={2}>
+                                            <TextField sx={{display: 'flex'}} inputProps={{...register(`orders.${index}.market`)}} label="Mercado" variant="outlined" InputLabelProps={{shrink: true}} />
+                                        </Grid>
+                                        <Grid item xs={12} sm={3} lg={1}>
+                                            <TextField sx={{display: 'flex'}} inputProps={{...register(`orders.${index}.buyOrSell`)}} label="C/V" variant="outlined" InputLabelProps={{shrink: true}} />
+                                        </Grid>
+                                        <Grid item xs={12} sm={3} lg={2}>
+                                            <TextField sx={{display: 'flex'}} inputProps={{...register(`orders.${index}.marketType`)}} label="Tipo mercado" variant="outlined" InputLabelProps={{shrink: true}} />
+                                        </Grid>
+                                        <Grid item xs={12} sm={6} lg={2}>
+                                            <TextField sx={{display: 'flex'}} inputProps={{...register(`orders.${index}.title`)}} label="Título" variant="outlined" InputLabelProps={{shrink: true}} />
+                                        </Grid>
+                                        <Grid item xs={12} sm={3} lg={1}>
+                                            <TextField sx={{display: 'flex'}} inputProps={{...register(`orders.${index}.quantity`)}} label="Quantidade" variant="outlined" InputLabelProps={{shrink: true}} />
+                                        </Grid>
+                                        <Grid item xs={12} sm={3} lg={1}>
+                                            <TextField sx={{display: 'flex'}} inputProps={{...register(`orders.${index}.price`)}} label="Preço" variant="outlined" InputLabelProps={{shrink: true}} />
+                                        </Grid>
+                                        <Grid item xs={12} sm={3} lg={1}>
+                                            <TextField sx={{display: 'flex'}} inputProps={{...register(`orders.${index}.total`)}} label="Total" variant="outlined"  InputLabelProps={{shrink: true}} />
+                                        </Grid>
+                                        <Grid item xs={12} sm={3} lg={1}>
+                                            <TextField sx={{display: 'flex'}} inputProps={{...register(`orders.${index}.debitOrCredit`)}} label="D/C" variant="outlined" InputLabelProps={{shrink: true}} />
+                                        </Grid>
+                                    </Grid>
                         )
                     })
                 }
+                <Box sx={{display: 'flex', justifyContent: 'center', my: -2}}>
+                    <IconButton size="small" sx={{display: 'flex'}} aria-label="fingerprint" color="success" onClick={onIncrementOrder}>
+                        <AddCircleIcon fontSize="large"/>
+                    </IconButton>
+                </Box>
             </Box>
         )
     }
